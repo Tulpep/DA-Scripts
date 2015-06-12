3ad5fb96f3915e9ae3eecf0b11d3636b9cf64726
Ricardo Polo <ricardo.polo@tulpep.com>
2015-06-12 16:27:36 -0500

    New functions added to test Ports

--- Main.ps1
+++ Main.ps1
@@ -2,8 +2,16 @@ Import-Module ".\CheckValidOS.ps1" -Force
 $VersionFromEnviroment = Get-ValidOSEnvironment
 Write-Host The OS according to the enviroment is: $VersionFromEnviroment
 $VersionFromWmi = Get-ValidOSWMI
-Write-Host The OS according to WMI is: $VersionFromWmi
 
+Write-Host The OS according to WMI is: $VersionFromWmi
 $HostName = Read-Host "Write the computer name"
-$VersionFromWMiRemote = Get-ValidOSWMIRemote  $
+$VersionFromWMiRemote = Get-ValidOSWMIRemote  $HostName
 Write-Host The OS accordint to WMI remote is: $VersionFromWMiRemote
+
+
+Import-Module ".\CheckPort.ps1" -Force
+
+$Pc = Read-Host Write the name of the server
+$Port = Read-Host Port to test
+$Protocol = Read-Host Protocol to test
+Check-Port $Pc $Port $Protocol
