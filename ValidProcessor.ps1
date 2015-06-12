a32e0a7cadad49b5442e25f75525420a4e7fe015
andresparrav <andresparrav@outlook.com>
2015-06-12 17:50:27 -0500

    Parra
    Validar procesador

---
+++ ValidProcessor.ps1
@@ -0,0 +1,26 @@
+﻿
+function ValidProcessor ()
+{
+    $cpu = Get-WmiObject Win32_Processor
+} 
+
+ValidProcessor
+
+
+
+
+<#write-host $cpu.name
+#Write-Host $cpu.caption
+$asd = "asd5ertytrasd"
+function asdfiasdfjh($asd)
+{
+
+Write-Host $asd
+
+
+
+
+}
+
+asdfiasdfjh(2 3)
+#>
